7270da2654b40e00dee24425bead40554bed3bec
michael <michaeleburrell@gmail.com>
2019-09-02 10:57:51 -0700

    adds updates to mini capstone app

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,14 +1,49 @@
 class Api::ProductsController < ApplicationController
 
-  def product_all
+  def index
     @products = Product.all
     # @message = "sanity check"
-    render "product_all.json.jb"
+    render "index.json.jb"
   end
 
-  def product_individual
-    @product = Product.all[1]   
+  def show
+    product_id = params[:id]
+    @product = Product.find_by(id: product_id)
+     # = params[0]
     # @message = "sanity check"
-    render "product_individual.json.jb"
+    render "show.json.jb"
   end
+
+  def create
+    @product = Product.new(
+      id: params[:id],
+      name: params[:name],
+      price: params[:price],
+      image_url: params[:image_url],
+      description: params[:description]
+      )
+
+    @product.save
+    render "create.json.jb"
+  end
+
+  def update
+    @product = Product.find_by(id: params[:id])
+
+    @product.name = params[:name] || @product.name
+    @product.price = params[:price].to_i || @product.price
+    @product.image_url = params[:image_url] || @product.image_url
+    @product.description = params[:description] || @product.description
+
+    @product.save
+    render "show.json.jb"
+  end
+
+  def destroy
+    product = Product.find_by(id: params[:id])
+    product.destroy
+    render json: {message: "gone."}
+  end
+
 end
+  
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,14 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
 
   namespace :api do
-    get "/products_all" => "products#product_all"
-    get "/product_individual" => "products#product_individual"
+    get "/products" => "products#index"
+
+    get "/products/:id" => "products#show"
+
+    post "/products" => "products#create"
+
+    patch "/products/:id" => "products#update"
+
+    delete "/products/:id" => "products#destroy"
   end
 end
