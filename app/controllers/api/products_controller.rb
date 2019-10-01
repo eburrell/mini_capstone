2d0e359ff52066eeca83c6fa3b1eed5432d0b277
michael <michaeleburrell@gmail.com>
2019-09-30 18:35:52 -0700

    comments out authenticate_admin

--- app/controllers/api/products_controller.rb
+++ app/controllers/api/products_controller.rb
@@ -1,6 +1,6 @@
 class Api::ProductsController < ApplicationController
 
-  before_action :authenticate_admin, except: [:index, :show]
+  # before_action :authenticate_admin, except: [:index, :show]
   
   def index
     
